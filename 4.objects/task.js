5b5ae4cf1f2f73e1e9129f58ad9213d99e9845c3
Lazarev_K_E <klik_1995@yahoo.com>
2026-08-28 13:09:34 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+  this.subject = subjectName;
+};
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+Student.prototype.addMarks = function (...marksToAdd) {
+  // Если свойства marks нет (студент отчислен) – ничего не делаем
+  if (!this.marks) {
+    return;
+  }
+  // Добавляем все переданные оценки в массив marks
+  this.marks.push(...marksToAdd);
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+  // Если marks не существует или массив пуст – возвращаем 0
+  if (!this.marks || this.marks.length === 0) {
+    return 0;
+  }
+  // Суммируем оценки и делим на их количество
+  const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+  return sum / this.marks.length;
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+  // Удаляем свойства subject и marks
+  delete this.subject;
+  delete this.marks;
+  // Добавляем свойство excluded с причиной отчисления
+  this.excluded = reason;
+};
